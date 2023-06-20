26f657aaccfd14ea97ea3e152e63a04ef6368e87
Fauzia1221 <fauzianaomi12@gmail.com>
2023-06-20 05:30:36 -0700

    Error_Correction

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -5,20 +5,13 @@
  */
 void print_alphabet_x10(void)
 {
-	char ch;
-	int i;
-	
-	i = 0;
+	int count = 0;
+	char letter;
 
-	while (i <=10)
+	while (count++ <= 9)
 	{
-		ch = 'a'
-		while (ch <= 'z')
-		{
-			_putchar(ch);
-			ch++;
-		}
-		_putchar('n');
-		i++;
+		for (letter = 'a'; letter <= 'z'; letter++)
+			_putchar(letter);
+		_putchar('\n');
 	}
 }
